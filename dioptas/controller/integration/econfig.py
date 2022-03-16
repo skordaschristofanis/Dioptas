be1aeaef493a527a37c32f2575653ba7de90b588
Christofanis Skordas <skordas.chr@hotmail.com>
2022-03-16 11:42:31 -0500

    Changed default IDD PVs.

--- dioptas/controller/integration/econfig.py
+++ dioptas/controller/integration/econfig.py
@@ -26,4 +26,4 @@ epics_config = {
     'sample_position_x': '13IDD:m81',
     'sample_position_y': '13IDD:m83',
     'sample_position_z': '13IDD:m82',
-    'sample_position_omega': '13IDD:m96'}
+    'sample_position_omega': '13IDD:m84'}
